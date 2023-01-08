475246bcf6ecc6079f309dfdd57afac3ad991325
kranks40 <ketoranks29@gmail.com>
2023-01-08 18:53:34 -0400

    created most important file structure

--- src/App.js
+++ src/App.js
@@ -4,16 +4,20 @@ import "./App.css";
 import Navbar from "./component/Navbar";
 import Home from "./component/pages/Home";
 import ExerciseDetail from "./component/pages/ExerciseDetail";
+import Footer from "./component/Footer";
 
 function App() {
   return (
-    <Box width="400px">
+    <Box width="400px" sx={{ width: {xl: '1488px'}}} m='auto'>
       {/* Navbar */}
       <Navbar />
 
       {/* Route */}
-      <Route path="/" element={<Home />} />
-      <Route path="/exercise/:id" element={<ExerciseDetail />} />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/exercise/:id" element={<ExerciseDetail />} />
+      </Routes>
+      <Footer />
     </Box>
   );
 }
